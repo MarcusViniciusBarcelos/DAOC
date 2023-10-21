044ad05021961658abb455c567e1bba38f7e31a4
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-21 01:08:09 -0300

    estudando hooks - useReducer

--- ap2/src/templates/Home/index.jsx
+++ ap2/src/templates/Home/index.jsx
@@ -1,11 +1,52 @@
-import { HomeContext } from '../../contexts/HomeContext';
+import { useReducer } from 'react';
 import './styles.css';
-import { Div } from '../../components/Div';
+
+const globalState = {
+  title: 'O título que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+
+  console.log('NENHUMA ACTION ENCONTRADA...');
+  return { ...state };
+};
 
 export function Home() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title } = state;
+
   return (
-    <HomeContext>
-      <Div />
-    </HomeContext>
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-BR'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+      <button onClick={() => dispatch({ type: 'QUALQUERCOiSA' })}>
+        SEM ACTION
+      </button>
+    </div>
   );
 }
