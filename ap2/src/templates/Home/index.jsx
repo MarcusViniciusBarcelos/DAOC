ef1afd4717c9ec9672589e46838f3c0fb9dee25d
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-20 21:24:07 -0300

    estudo de hooks - useRef

--- ap2/src/templates/Home/index.jsx
+++ ap2/src/templates/Home/index.jsx
@@ -1,12 +1,12 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './styles.css';
 
-export const Post = ({ post }) => {
+export const Post = ({ post, handleClick }) => {
   console.log('Filho renderizou');
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
       <p>{post.body}</p>
     </div>
   );
@@ -18,10 +18,17 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.func,
 };
 export function Home() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   console.log('Pai renderizou');
 
@@ -30,10 +37,21 @@ export function Home() {
       .then((r) => r.json())
       .then((r) => setPosts(r));
   }, []);
+
+  useEffect(() => {
+    console.log(input.current);
+    input.current.focus();
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  }, []);
   return (
     <div className="home">
+      <h1>Renderizou: {contador.current} vezes</h1>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -42,7 +60,9 @@ export function Home() {
       {useMemo(() => {
         return (
           posts.length > 0 &&
-          posts.map((post) => <Post key={post.id} post={post} />)
+          posts.map((post) => (
+            <Post key={post.id} post={post} handleClick={handleClick} />
+          ))
         );
       }, [posts])}
       {posts.length <= 0 && (
