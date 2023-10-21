b68a1b60f6a51334b3dd8ceaae9bd3c63b61bc09
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-20 21:02:30 -0300

    estudando hooks - useMemo

--- ap2/src/templates/Home/index.jsx
+++ ap2/src/templates/Home/index.jsx
@@ -1,29 +1,55 @@
 import P from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
 import './styles.css';
-import React, { useCallback, useState } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('Filho, renderizou');
-  return <button onClick={() => incrementButton(10)}>+</button>;
-});
-
-Button.propTypes = {
-  incrementButton: P.func,
+export const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+};
 export function Home() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  console.log('Pai renderizou');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
   }, []);
-
-  console.log('Pai, renderizou');
   return (
-    <div className="App">
-      <p>Teste 2</p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+    <div className="home">
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => <Post key={post.id} post={post} />)
+        );
+      }, [posts])}
+      {posts.length <= 0 && (
+        <div className="post">
+          <h1>Carregando...</h1>
+        </div>
+      )}
     </div>
   );
 }
