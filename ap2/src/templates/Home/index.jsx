f4b5516aa087eb82d5e661b589aab6c2ab354498
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-10-28 12:23:33 -0300

    estudando hooks - React.lazy - Suspense e divisão de código (Code-Splitting)

--- ap2/src/templates/Home/index.jsx
+++ ap2/src/templates/Home/index.jsx
@@ -1,24 +1,16 @@
-import PropTypes from 'prop-types';
-import React, {
-  Children,
-  Suspense,
-  cloneElement,
-  useCallback,
-  useDebugValue,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles.css';
 
-const LazyComponent = React.lazy(() => import('./LazyComponent'));
+const loadComponent = () => import('./LazyComponent');
+const LazyComponent = React.lazy(loadComponent);
 
 export function Home() {
   const [show, setShow] = useState(false);
   return (
     <div>
-      <button onClick={() => setShow((s) => !s)}>Show</button>
+      <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+        Show
+      </button>
       <Suspense fallback={<p>Loading...</p>}>
         {show && <LazyComponent />}
       </Suspense>
