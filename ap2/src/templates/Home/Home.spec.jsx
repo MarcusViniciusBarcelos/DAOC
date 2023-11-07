a0f2f517bf7bec802d75c8e42e19155bd4edb5fc
Marcus Barcelos <marcusbarcelos2001@gmail.com>
2023-11-07 18:42:13 -0300

    .

--- ap2/src/templates/Home/Home.spec.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '.';
-
-it('dump test', () => {
-  render(<Home />);
-  expect(screen.getByText('Home')).toBeInTheDocument();
-});
